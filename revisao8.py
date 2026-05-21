65edd1870ba4d9079d0fb69b7e39f3051d998ed0
alisonfschikorski <alisonfschikorski@gmail.com>
2026-05-20 21:53:42 -0300

    revi

---
+++ revisao8.py
@@ -0,0 +1,7 @@
+cidades= ["blumenau", "indaial", "gaspar" , "pouso redondo" , "pomerode"]
+
+for cidades in cidades: 
+    #print(f"as cidades no sistema sao: {cidades}")
+    if cidades[0] == "b":
+        continue
+    print(cidades)
